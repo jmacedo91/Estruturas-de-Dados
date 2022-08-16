ce247e636525abb5f0ce6fcfc31483fcf966a23c
Jonathan Macedo <88295865+jmacedo91@users.noreply.github.com>
2022-08-15 22:41:31 -0300

    Add files via upload

---
+++ Curso_C++/01_Primeiro_Algoritmo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!"<< endl;
+    cout << "Hello World!\nCanal Professor Douglas Maioli"; // pode usar o \n, como no python
+
+    system("pause");
+
+    return 0;
+}
