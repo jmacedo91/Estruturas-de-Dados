d7180785d4b2bb44cd7aa29c7f1b92db0752d4ee
Jonathan Macedo <88295865+jmacedo91@users.noreply.github.com>
2022-08-31 20:50:20 -0300

    Add files via upload

---
+++ Estruturas de Dados/pilha.h
@@ -0,0 +1,21 @@
+// pilha = stack
+typedef int TipoItem;
+const int max_itens = 100;
+
+class pilha{
+
+    private:
+    int tamanho;
+    TipoItem* estrutura;
+
+    public:
+    pilha(); //construtora //stack;
+    ~pilha(); //destrutora //~stack
+    bool estacheia(); //verifica se apilha esta cheia //isfull
+    bool estavazia(); //verifica se a pilha esta vazia //isempty
+    void inserir(TipoItem item); //push
+    TipoItem remover(); //pop
+    void imprimir(); //print
+    int qualtamanho(); //lenght
+
+};
