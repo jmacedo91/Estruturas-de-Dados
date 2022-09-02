b69daf8ccdaf0ef33478002a382ce11af32e1451
Jonathan Macedo <88295865+jmacedo91@users.noreply.github.com>
2022-09-02 15:39:55 -0300

    Add files via upload

---
+++ Curso_C++/Desafio_06.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    float gastos[5];
+    float medgastos;
+    float total = 0;
+    
+    cout << "Desafio 6 - Curso C++\n\n";
+    
+    cout << "Digite o gasto da primeira familia: " << endl;
+    cin >> gastos[0];
+    
+    cout << "Digite o gasto da segunda familia: " << endl;
+    cin >> gastos[1];
+    
+    cout << "Digite o gasto da terceira familia: " << endl;
+    cin >> gastos[2];
+    
+    cout << "Digite o gasto da quarta familia: " << endl;
+    cin >> gastos[3];
+    
+    cout << "Digite o gasto da quinta familia: " << endl;
+    cin >> gastos[4];
+    
+    for (int i = 0; i < 5; i++) {
+        total += gastos[i];
+    }
+    
+    medgastos = total / 5;
+    
+    cout << "\nA media de gastos entre as familias foi de R$: " << medgastos << "\n\n";
+    
+    for (int i = 0; i < 5; i++) {
+        if (gastos[i] > medgastos) {
+            cout << "A familia " << i + 1 << " gastou acima da media." << endl;
+        } else if (gastos[i] < medgastos) {
+            cout << "A familia " << i + 1 << " gastou abaixo da media." << endl;
+        } else {
+            cout << "A familia " << i + 1 << " gastou na media." << endl;
+        }
+    }
+
+    return 0;
+}
